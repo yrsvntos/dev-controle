7052e44b21f4f1dd6b984bcc2a54f674713e292e
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-22 19:11:17 +0100

    feat(header): adicionar a funcionalidade do login e logout no componente

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,53 @@
+"use client"
+
 import Link from "next/link";
-import { FiLogOut, FiUser } from "react-icons/fi";
+import { FiLoader, FiLock, FiLogOut, FiUser } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Header(){
+    const {data, status} = useSession();
+
+    console.log(status)
+    async function handleLogin(){
+        await signIn();
+        
+    }
+
+    async function handleLogout() {
+        await signOut()
+    }
+
     return(
         <header className="w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm">
            <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
-                <Link href="">
+                <Link href="/">
                     <h1 className="font-bold text-2xl pl-1 hover:tracking-widest duration-300"><span className="text-blue-500">DEV</span> CONTROLE</h1>
                 </Link>
 
-                <div className="flex items-baseline gap-4 ">
-                    <Link href="/login">
+                {status === "loading" && (
+                    <button>
+                        <FiLoader className="animate-spin" size={26} color="4b5563"/>
+                    </button>
+                )}
+
+                {status === "unauthenticated" && (
+                    <button
+                        onClick={handleLogin}
+                    >
+                        <FiLock size={26} color="4b5563"/>
+                    </button>
+                )}
+               
+               {status === "authenticated" && (
+                 <div className="flex items-baseline gap-4 ">
+                    <Link href="/dashboard">
                         <FiUser size={20} color="#4b5563"/>
                     </Link>
-                    <button>
-                        <Link href="/login">
-                            <FiLogOut size={20} color="#4b5563"/>
-                        </Link>
+                    <button onClick={handleLogout}> 
+                        <FiLogOut size={20} color="#ff3a13"/> 
                     </button>
                 </div>
+               )}
            </div>
         </header>
     )
