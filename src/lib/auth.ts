6fd0bbf975859a208b36a5cb8bafa9b7688ebf97
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-22 19:17:05 +0100

    feat(auth):fornecer utilitários para a autenticação de usuários

---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { AuthOptions } from "next-auth";
+import PrismaClient from "./prisma";
+import { Adapter } from "next-auth/adapters";
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(PrismaClient),
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ],
+    callbacks: {
+        async session({session, token, user}){
+            session.user = {...session.user,id: user.id} as {
+                id: string;
+                name: string;
+                email: string;
+            }
+            return session
+        },
+        async redirect({ url, baseUrl }) {
+            
+            return '/dashboard';
+          },
+
+        
+    }
+}
