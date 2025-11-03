fe1772f3ec31a24e5d00ac6b678c30f54d1a6afc
Yanik dos Santos <yrsvntos@gmail.com>
2025-11-03 20:36:26 +0000

    feat: adicionar o Modal Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { AuthProvider } from "@/providers/auth";
+import { ModalProvider } from "@/providers/modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +29,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header/>
-        {children}
+      > 
+        <AuthProvider>
+          <ModalProvider>
+            <Header/>
+            {children}
+          </ModalProvider>
+        </AuthProvider>
       </body>
     </html>
   );
